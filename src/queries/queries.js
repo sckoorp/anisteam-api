5e144e9c2139975523ce4090f85e7f031f35392a
sckoorp <ckamalennour@gmail.com>
2024-05-28 17:20:03 +0100

    update: added all time popular anime route /popular

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -27,3 +27,33 @@ export const TrendingQuery =
             }
         }
     }`
+
+export const PopularQuery =
+    (page, perPage) => `query {
+        Page(page: ${page}, perPage: ${perPage}) {
+            pageInfo {
+                currentPage
+                hasNextPage
+            }
+            media(type: ANIME, sort: POPULARITY_DESC) {
+                id
+                title {
+                    romaji
+                }
+                coverImage {
+                    extraLarge
+                }
+                format
+                seasonYear
+                averageScore
+                episodes
+                description(asHtml: true)
+                studios(isMain: true) {
+                    nodes {
+                      name
+                    }
+                }
+                genres
+            }
+        }
+    }`
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { getTrending } from "../modules/modules.js";
+import { getTrending, getPopular } from "../modules/modules.js";
 
 router.get("/trending", async (request, response) => {
     try {
@@ -11,6 +11,24 @@ router.get("/trending", async (request, response) => {
             code: response.statusCode,
             type: "OK",
             data: await getTrending(page, per)
+        });
+    } catch (error) {
+        return response.status(500).json({
+            code: response.statusCode,
+            type: "Internal Server Error",
+            message: error.message
+        });
+    }
+});
+
+router.get("/popular", async (request, response) => {
+    try {
+        const page = request.query.page || 1
+        const per = request.query.per || 50
+        return response.status(200).json({
+            code: response.statusCode,
+            type: "OK",
+            data: await getPopular(page, per)
         });
     } catch (error) {
         return response.status(500).json({
